1cec91ad23a862b93577f0ba06b864532e5d49fd
Jake Soenneker <jake@soenneker.com>
2025-03-26 09:12:51 -0500

    Initial commit

---
+++ test/Soenneker.Attributes.Validation.Guid.Nullable.Tests/NullableGuidValidationAttributeTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Attributes.Validation.Guid.Nullable.Tests;
+
+public sealed class NullableGuidValidationAttributeTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
